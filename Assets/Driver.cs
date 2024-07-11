1f4a660318bfb9573a3c06bbb751300c6e230bb4
Jace Weerawardena <jasonweerawardena@gmail.com>
2024-07-11 18:42:57 +0100

    boost system added

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -8,16 +8,30 @@ public class Driver : MonoBehaviour
     //our variable declarations must be inside the class
     [SerializeField] float steerSpeed = 1f; //the speed of our steering
     [SerializeField] float moveSpeed = 0.01f; //the speed our car is moving (backwards and forwards)
+    [SerializeField] float boostSpeed = 0.01f; //Speed our car goes when our car runs over a boost
+
+    //when touching boost double move speed
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Boost"){
+            moveSpeed = boostSpeed;
+            Destroy(other.gameObject);
+        }
+    }
+
+    //slow down from boost after collision with object
+    private void OnCollisionEnter2D(Collision2D other) {
+        if (moveSpeed >= 30){
+            moveSpeed = moveSpeed / 2;
+        }
+    }
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
 
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         /*
             every frame calculate the amount the car is steering
             It is then mutiplied by steerSpeed so we are able to
